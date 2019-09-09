5372c69feb0a04555ad8450a867cbc6fa69b693f
ybil <55065499+ybil@users.noreply.github.com>
2019-09-09 13:58:58 +0300

    Update index.py

--- test-master/hello-app/dynamic/index.py
+++ test-master/hello-app/dynamic/index.py
@@ -8,7 +8,7 @@ app = web.application(urls, globals())
 
 class hello:
 	def GET(self):
-		return 'Hello World from dynamic page!'
+		return 'Hello World'
 
 if __name__ == "__main__":
 	web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
